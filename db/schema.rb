f9820cc85a7bbab5179560cbb43e594b3814715f
Davidm8624 <davidm8624@gmail.com>
2023-09-30 15:20:03 -0700

    migrated DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_18_191505) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_30_221824) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -21,4 +21,24 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_18_191505) do
     t.string "location"
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "name"
+    t.date "birthday"
+    t.text "bio"
+    t.string "color"
+    t.string "X"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "profiles", "users"
 end
