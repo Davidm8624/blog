85fbfd45cc47974054463baa24ab35aadcf90698
Davidm8624 <davidm8624@gmail.com>
2023-09-30 15:14:15 -0700

    user model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
   validates :title, :body, presence: true #makes it where you cant submit something blank
+
+  def long_title
+    "#{title} - #{published_at}"
+  end
 end
