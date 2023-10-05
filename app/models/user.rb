42f6c7decb3774438cda06fc18047ce2b8c16650
Davidm8624 <davidm8624@gmail.com>
2023-10-05 10:34:31 -0700

    was wrong about last commit, everything was working fine. The reason there was a problem was becase i set user = User.frist then updated the User. user was still on the only User and didnt update with new User content.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,3 +3,4 @@ class User < ApplicationRecord
   has_many :articles, -> {order 'published_at DESC, title ASC'}, dependent: :nullify          #makes it where the newset publiched articles for that user will be at the top and not the bottom. if the user is deleted the articles that belong to the user willbe destroyied aswell. you can set it yo :nullify if you want the articlesto remain but without a publisher
   has_many :replies, through: :articles, source: :comments
 end
+
